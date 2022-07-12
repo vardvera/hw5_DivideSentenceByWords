a1d22fc9ed602ed94efcd5fafaf1e95597012bff
Vera Vardanyan <vardvera@gmail.com>
2022-07-12 21:23:41 +0300

    split sentence + reverse sentence

---
+++ hw5_StringSplit/Methods.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+
+
+
+
+
+namespace hw5_StringSplit
+{
+    class Methods
+    {
+
+        public enum ReverseOption
+        {
+            No,
+            Yes
+        }
+
+        public static void PrintWordsOnLines(string [] words) 
+        {
+            foreach (var e in words) { Console.WriteLine($"{e}"); }
+        }
+
+        public static void ReverseWords(string [] words)
+        {
+            int wordCount = 1;
+            string reverseWords = "";
+
+            foreach (var e in words) { reverseWords += words[words.Length - wordCount] + " "; wordCount++; }
+            Console.WriteLine($"{reverseWords}");
+        }
+
+        public static void SplitSentenceByWords(string Sentence, ReverseOption options)
+        {
+            char[] charSeparators = new char[] { ' ' };
+            string[] words;
+
+            words = Sentence.Split(charSeparators, StringSplitOptions.RemoveEmptyEntries);
+
+            if (options == 0 ) { PrintWordsOnLines(words); }
+            else { ReverseWords(words);}
+        }
+
+        
+ 
+           
+    }
+}
--- hw5_StringSplit/Program.cs
+++ hw5_StringSplit/Program.cs
@@ -1,33 +1,34 @@
 ﻿using System;
 
+
+
+
 namespace hw5_StringSplit
 {
     class Program
     {
-        static void PrintWordsOnLines(string [] words) 
+        static void Main (string[] args)
         {
-            foreach (var e in words) { Console.WriteLine($"{e}"); }
-        }
-
-        static void SplitSentenceByWords(string Sentence)
-        {
-            char[] charSeparators = new char[] { ' ' };
-            string[] words;
-
-            words = Sentence.Split(charSeparators, StringSplitOptions.RemoveEmptyEntries);
-
-            PrintWordsOnLines(words);
-        }
-
- 
-        static void Main(string[] args)
-        {
+            // печать каждого слова в предложении на новой строчке
+            
             Console.WriteLine("Введите предложение с единичным пробелом между словами:");
             string Sentence = Console.ReadLine();
 
-            SplitSentenceByWords(Sentence);
+            Methods.SplitSentenceByWords(Sentence, Methods.ReverseOption.No);
 
+            Sentence = "";
+            Console.WriteLine();
+
+
+
+            // перестановка слов местами в обратной последовательности
+
+            Console.WriteLine("Введите предложение с единичным пробелом между словами:");
+            Sentence = Console.ReadLine();
+
+            Methods.SplitSentenceByWords(Sentence, Methods.ReverseOption.Yes);
         }
-           
+
+
     }
 }
