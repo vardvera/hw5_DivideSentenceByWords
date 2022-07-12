f02575069d092fe9f9850967f1dee56cdbd62f27
Vera Vardanyan <vardvera@gmail.com>
2022-07-12 19:41:05 +0300

    Добавьте файлы проекта.

---
+++ hw5_StringSplit/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace hw5_StringSplit
+{
+    class Program
+    {
+        static void PrintWordsOnLines(string [] words) 
+        {
+            foreach (var e in words) { Console.WriteLine($"{e}"); }
+        }
+
+        static void SplitSentenceByWords(string Sentence)
+        {
+            char[] charSeparators = new char[] { ' ' };
+            string[] words;
+
+            words = Sentence.Split(charSeparators, StringSplitOptions.RemoveEmptyEntries);
+
+            PrintWordsOnLines(words);
+        }
+
+ 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение с единичным пробелом между словами:");
+            string Sentence = Console.ReadLine();
+
+            SplitSentenceByWords(Sentence);
+
+        }
+           
+    }
+}
